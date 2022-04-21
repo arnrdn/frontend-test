90b04f97cb3a26393f9956b8ab5091140f0f0fb3
rdnrn <rodina.arina00@gmail.com>
2022-04-21 23:52:39 +0300

    Setup the project with Vue CLI

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <card-list/>
+  </div>
+</template>
+
+<script>
+import CardList from './components/CardList.vue'
+export default {
+  components: {CardList},
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li class="list__item">
+  <img src="item.imgLink" alt="">
+  <h3>{{item.title}}</h3>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
